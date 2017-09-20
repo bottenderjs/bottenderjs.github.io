1aa8c49c66c202b027862a9984d2073374d8cc6e
Homer Chen <xxhomey19@gmail.com>
2017-09-20 17:30:41 +0800

    add build-sidebar (#2)

---
+++ build-sidebar.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const fs = require('fs');
+
+const glob = require('glob-promise');
+const decamelize = require('decamelize');
+const stringifyObject = require('stringify-object');
+
+const logger = console;
+
+async function main() {
+  const docs = [];
+  const docsFiles = await glob('docs/**', { ignore: ['docs'] });
+
+  await Promise.all(
+    docsFiles.map(async file => {
+      const result = file.match(/(.*)\/(.*)-(.*).md/);
+      const doc = { name: result[2].replace('API', 'API '), posts: [] };
+
+      const post = {
+        name: result[3] === 'GettingStarted' ? 'Getting Started' : result[3],
+        href: `/docs/${decamelize(result[2], '-')}/${decamelize(
+          result[3],
+          '-'
+        )}`,
+      };
+
+      if (result[3] === 'GettingStarted') {
+        post.aliases = ['/docs'];
+      }
+
+      doc.posts.push(post);
+
+      docs.push(doc);
+    })
+  );
+
+  fs.writeFileSync(
+    path.resolve(__dirname, `lib/data/docs.js`),
+    `// prettier-ignore\nexport default ${stringifyObject(docs, {
+      indent: '  ',
+    })};\n`
+  );
+
+  logger.log('overwrite lib/data/docs.js successfully');
+}
+
+main();
