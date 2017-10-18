73c4a214eeaee06b9cfa676d3c6426bf6fe580b2
Homer Chen <xxhomey19@gmail.com>
2017-10-18 15:00:11 +0800

    normalize css (#16)

---
+++ src/css/index.js
@@ -0,0 +1,6 @@
+import styledNormalize from 'styled-normalize';
+import { injectGlobal } from 'styled-components';
+
+export default () => injectGlobal`
+  ${styledNormalize}
+`;
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import baseStyles from '../css';
 import Sidebar from '../components/Sidebar';
 import Header from '../components/Header';
 import { rhythm } from '../utils/typography';
@@ -15,15 +16,19 @@ const Container = styled.div`
   display: flex;
 `;
 
-export default ({ children, data }) => (
-  <Wrapper>
-    <Header title={data.site.siteMetadata.title} />
-    <Container>
-      <Sidebar />
-      {children()}
-    </Container>
-  </Wrapper>
-);
+export default ({ children, data }) => {
+  baseStyles();
+
+  return (
+    <Wrapper>
+      <Header title={data.site.siteMetadata.title} />
+      <Container>
+        <Sidebar />
+        {children()}
+      </Container>
+    </Wrapper>
+  );
+};
 
 export const query = graphql`
   query LayoutQuery {
