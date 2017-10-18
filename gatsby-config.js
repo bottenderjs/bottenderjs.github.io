8a173ffda0119f1bdfaa07c02332825c196c70aa
Homer Chen <xxhomey19@gmail.com>
2017-10-18 14:11:06 +0800

    add google analytics (#17)

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,11 @@ module.exports = {
         pathToConfigModule: 'src/utils/typography',
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: 'UA-108095811-1',
+      },
+    },
   ],
 };
