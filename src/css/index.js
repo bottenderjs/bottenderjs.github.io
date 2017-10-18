987ac08f2513310b6c5012af763c38c2edaa41ec
Homer Chen <xxhomey19@gmail.com>
2017-10-18 17:11:47 +0800

    overwrite font-family (#24)

--- src/css/index.js
+++ src/css/index.js
@@ -1,6 +1,32 @@
 import styledNormalize from 'styled-normalize';
 import { injectGlobal } from 'styled-components';
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Karla', 'Roboto'],
+  },
+});
 
 export default () => injectGlobal`
   ${styledNormalize}
+
+  body {
+    font-family: Karla, Roboto, sans-serif;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  a,
+  button {
+    font-family: Karla;
+  }
+
+  p {
+    font-family: Roboto;
+  }
 `;
