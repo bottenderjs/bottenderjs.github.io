bb53eb8235c650d0d80f78f9086cf7dc54da744c
吳東曄 Wu, Dung-Ie <tw0517tw@gmail.com>
2017-10-28 11:23:05 +0800

    no exhibition (#116)
    fix #112

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 
 import LandscapeSection from '../components/home/LandscapeSection';
 import IntroductionSection from '../components/home/IntroductionSection';
-import ExhibitionSection from '../components/home/ExhibitionSection';
 import CTASection from '../components/home/CTASection';
 
 const Wrapper = styled.div`
@@ -16,7 +15,6 @@ export default () => (
   <Wrapper>
     <LandscapeSection />
     <IntroductionSection />
-    <ExhibitionSection />
     <CTASection />
   </Wrapper>
 );
